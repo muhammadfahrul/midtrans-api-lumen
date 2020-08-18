7a135d0243391ca69e02fda1208c95bc2b044166
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-08-18 13:40:15 +0700

    midtrans-api-lumen

--- routes/web.php
+++ routes/web.php
@@ -15,43 +15,41 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['middleware' => 'authjwt'], function () use ($router) {
-    $router->group(['prefix' => 'api/v1'], function () use ($router) {
-        $router->get('/customer', 'CustomerController@showAll');
-        $router->get('/customer/{id}', 'CustomerController@showId');
-        $router->post('/customer', 'CustomerController@add');
-        $router->put('/customer/{id}', 'CustomerController@update');
-        $router->delete('/customer/{id}', 'CustomerController@delete');
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->get('/customer', 'CustomerController@showAll');
+    $router->get('/customer/{id}', 'CustomerController@showId');
+    $router->post('/customer', 'CustomerController@add');
+    $router->put('/customer/{id}', 'CustomerController@update');
+    $router->delete('/customer/{id}', 'CustomerController@delete');
 
-        $router->get('/order', 'OrderController@showAllJoin');
-        // $router->get('/order-join', 'OrderController@showAllJoin');
-        $router->get('/order/{id}', 'OrderController@showIdJoin');
-        // $router->get('/order-join/{id}', 'OrderController@showIdJoin');
-        $router->post('/order', 'OrderController@add');
-        $router->put('/order/{id}', 'OrderController@update');
-        $router->delete('/order/{id}', 'OrderController@delete');
+    $router->get('/order', 'OrderController@showAllJoin');
+    // $router->get('/order-join', 'OrderController@showAllJoin');
+    $router->get('/order/{id}', 'OrderController@showIdJoin');
+    // $router->get('/order-join/{id}', 'OrderController@showIdJoin');
+    $router->post('/order', 'OrderController@add');
+    $router->put('/order/{id}', 'OrderController@update');
+    $router->delete('/order/{id}', 'OrderController@delete');
 
-        // $router->get('/order-item', 'OrderItemController@showAll');
-        // $router->get('/order-item-join', 'OrderItemController@showAllJoin');
-        // $router->get('/order-item/{id}', 'OrderItemController@showId');
-        // $router->get('/order-item-join/{id}', 'OrderItemController@showIdJoin');
-        // $router->post('/order-item', 'OrderItemController@add');
-        // $router->put('/order-item/{id}', 'OrderItemController@update');
-        // $router->delete('/order-item/{id}', 'OrderItemController@delete');
-    
-        $router->get('/product', 'ProductController@showAll');
-        $router->get('/product/{id}', 'ProductController@showId');
-        $router->post('/product', 'ProductController@add');
-        $router->put('/product/{id}', 'ProductController@update');
-        $router->delete('/product/{id}', 'ProductController@delete');
-    
-        $router->get('/payment', 'PaymentController@showAll');
-        // $router->get('/payment-join', 'PaymentController@showAllJoin');
-        $router->get('/payment/{id}', 'PaymentController@showId');
-        // $router->get('/payment-join/{id}', 'PaymentController@showIdJoin');
-        $router->post('/payment', 'PaymentController@add');
-        $router->put('/payment/{id}', 'PaymentController@update');
-        $router->delete('/payment/{id}', 'PaymentController@delete');
-        $router->post('/payment/midtrans/push', 'PaymentController@midtransPush');
-    });
+    // $router->get('/order-item', 'OrderItemController@showAll');
+    // $router->get('/order-item-join', 'OrderItemController@showAllJoin');
+    // $router->get('/order-item/{id}', 'OrderItemController@showId');
+    // $router->get('/order-item-join/{id}', 'OrderItemController@showIdJoin');
+    // $router->post('/order-item', 'OrderItemController@add');
+    // $router->put('/order-item/{id}', 'OrderItemController@update');
+    // $router->delete('/order-item/{id}', 'OrderItemController@delete');
+
+    $router->get('/product', 'ProductController@showAll');
+    $router->get('/product/{id}', 'ProductController@showId');
+    $router->post('/product', 'ProductController@add');
+    $router->put('/product/{id}', 'ProductController@update');
+    $router->delete('/product/{id}', 'ProductController@delete');
+
+    $router->get('/payment', 'PaymentController@showAll');
+    // $router->get('/payment-join', 'PaymentController@showAllJoin');
+    $router->get('/payment/{id}', 'PaymentController@showId');
+    // $router->get('/payment-join/{id}', 'PaymentController@showIdJoin');
+    $router->post('/payment', 'PaymentController@add');
+    $router->put('/payment/{id}', 'PaymentController@update');
+    $router->delete('/payment/{id}', 'PaymentController@delete');
+    $router->post('/payment/midtrans/push', 'PaymentController@midtransPush');
 });
